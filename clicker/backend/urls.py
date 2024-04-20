f649329eba7bc70cca13f5bf978b24ef056d7f4f
Egor <egorbadauch@mail.ru>
2024-04-20 19:27:40 +0500

    boost upgrade

--- clicker/backend/urls.py
+++ clicker/backend/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, include
 from . import views
 
+boosts = views.BoostViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
 
-boosts = views.BoostViewSet.as_view({'get': 'list','post' : 'create'})
+
+lonely_boost = views.BoostViewSet.as_view({
+    'put' : 'partial_update',
+})
 
 
 urlpatterns = [
     path('', include('auth_clicker.urls')),
     path('call_click/', views.call_click, name='call_click'),
     path('boosts/', boosts, name='boosts'),
+    path('boost/<int:pk>/', lonely_boost, name='boost')
 ]
 
--- clicker/frontend/static/js/index.js
+++ clicker/frontend/static/js/index.js
@@ -1,21 +1,23 @@
-function call_click(){
-    fetch('/call_click',{
-        method : 'GET'
+function call_click() {
+    fetch('/call_click', {
+        method: 'GET'
     }).then(response => {
-        if (response.ok){
+        if (response.ok) {
             return response.json()
         }
-        return Promise.reject(response)
 
-    }).then(data => {
-        document.getElementById('coins').innerText = data.core.coins
-    }).catch(error => console.log(error))
+        return Promise.reject(response)
+        }).then(data => {
+            document.getElementById('coins').innerText = data.core.coins
+            if (data.is_levelup) {
+                get_boosts()
+            }
+        }).catch(error => console.log(error))
 }
 
-
-function get_boosts(){
+function get_boosts() {
     fetch('/boosts/', {
-        method : 'GET'
+        method: 'GET'
     }).then(response => {
         if (response.ok) {
             return response.json()
@@ -24,23 +26,38 @@ function get_boosts(){
     }).then(boosts => {
         const panel = document.getElementById('boosts-holder')
         panel.innerHTML = ''
-        boosts.foreach(boost => {
+        boosts.forEach(boost => {
             add_boost(panel, boost)
         })
     }).catch(error => console.log(error))
 }
 
-function add_boost(parent, boost){
+function add_boost(parent, boost) {
     const button = document.createElement('button')
     button.setAttribute('class', 'boost')
     button.setAttribute('id', `boost_${boost.id}`)
     button.setAttribute('onclick', `buy_boost(${boost.id})`)
     button.innerHTML = `
-        <p>lvl: <span id="boost_level">${boost.level}</span></p>
+        <p>Уровень: <span id="boost_level">${boost.level}</span></p>
         <p>+<span id="boost_power">${boost.power}</span></p>
-        <p><span id="boost_price">${boost.price}</span></p>
+        <p>Цена: <span id="boost_price">${boost.price}</span></p>
     `
     parent.appendChild(button)
+}
+
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
 
 /** Функция покупки буста */
@@ -70,17 +87,10 @@ function buy_boost(boost_id) {
     }).catch(err => console.log(err))
 }
 
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
+/** Функция для обновления буста на фронтике */
+function update_boost(boost) {
+    const boost_node = document.getElementById(`boost_${boost.id}`)
+    boost_node.querySelector('#boost_level').innerText = boost.level
+    boost_node.querySelector('#boost_power').innerText = boost.power
+    boost_node.querySelector('#boost_price').innerText = boost.price
 }
